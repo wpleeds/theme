a4875afd22f9e20ac70924c275988fd591c48de0
Sam Berson <sam@samberson.co.uk>
2014-08-20 17:41:30 +0100

    Remove unneccesary links for v1

--- header.php
+++ header.php
@@ -12,14 +12,10 @@
 
 		<link href="http://fonts.googleapis.com/css?family=Open+Sans:300,600,700" rel="stylesheet" />
 
-		<script src="<?php echo get_template_directory_uri(); ?>/js/jquery.min.js"></script>
-		<script src="<?php echo get_template_directory_uri(); ?>/js/config.js"></script>
-		<script src="<?php echo get_template_directory_uri(); ?>/js/skel.min.js"></script>
-		<noscript>
-			<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/skel-noscript.css" />
-			<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/style.css" />
-			<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/style-desktop.css" />
-		</noscript>
+		<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/skel-noscript.css" />
+		<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/style.css" />
+		<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/style-desktop.css" />
+
 		<!--[if lte IE 8]>
 			<script src="<?php echo get_template_directory_uri(); ?>/js/html5shiv.js"></script>
 			<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/ie8.css" />
