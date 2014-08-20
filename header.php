e160df051c9a9c17a5294df0194dc476e8b8e477
Sam Berson <sam@samberson.co.uk>
2014-08-20 17:44:21 +0100

    Add navbar and header wrapper as html

--- header.php
+++ header.php
@@ -28,3 +28,39 @@
 </head>
 
 <body>
+
+	<!-- Nav -->
+	<nav id="nav">
+		<ul>
+			<li><a href="#top"><i class="icon icon-heart"></i> Welcome</a></li>
+			<li><a href="#who"><i class="icon icon-user"></i> This Month</a></li>
+			<li><a href="#venue"><i class="icon icon-home"></i> Venue</a></li>
+			<li><a href="#structure"><i class="icon icon-time"></i> Structure & Topics</a></li>
+			<li><a href="#groups"><i class="icon icon-user"></i> Other Groups</a></li>
+			<li><a href="#social"><i class="icon icon-star"></i> Be Social</a></li>		
+		</ul>
+	</nav>
+
+	<!-- Home -->
+	<div class="wrapper wrapper-style1 wrapper-first">
+		<article class="container" id="top">
+			<div class="row">
+				<div class="5u">
+					<span class="intro-image image image-full"><img src="<?php echo get_template_directory_uri(); ?>/images/wpmess.png" alt="WordPress logo" /></span>
+				</div>
+				<div class="7u">
+					<header>
+						<h1><?php bloginfo('name'); ?></h1>
+					</header>
+
+					<p>WordPress Leeds is an informal organisation to help promote WordPress and Blogging events in Leeds and West Yorkshire. Our main event Northern Bloggers, is a monthly meetup for Bloggers, Journalists, Designers, Developers and anyone interested in Blogging and Digital Publishing.</p>
+
+					<div>
+						<p>Northern Bloggers is 6:30pm on the 4<sup>th</sup> Tuesday of the month at <a href="#venue" title="Get directions!">Duke Studios</a>. Due to space constraints the event is normally ticketed but is free!</p>
+					</div>
+					<a href="http://www.eventbrite.com/e/northern-bloggers-wordpress-leeds-tickets-12541964353?aff=wpleedssite" class="button button-big">Get a ticket for this months event</a> 
+
+				</div>
+			</div>
+		</article>
+	</div>
--- index.php
+++ index.php
@@ -1,5 +1,3 @@
 <?php get_header(); ?>
 
-<h1><?php bloginfo('name'); ?></h1>
-
 <?php get_footer();?>
