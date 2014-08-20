394163e43c4481c2dbe008e5e269b79762ec7f64
Sam Berson <sam@samberson.co.uk>
2014-08-20 17:55:16 +0100

    Add footer section to footer.php

--- footer.php
+++ footer.php
@@ -1,4 +1,71 @@
+<!-- Social + footer -->
+<div class="wrapper wrapper-style4">
+	<article id="social">
+		<header>
+			<h1>Stay in touch & be social</h1>
+		</header>
+		<div class="container">
+
+			<div class="row">
+
+				<div class="5u -1u txt-left">
+
+				<h3>Mailing list...</h3>
+
+				<!-- Begin MailChimp Signup Form -->
+				<div id="mc_embed_signup">
+					<form action="http://wpleeds.us7.list-manage1.com/subscribe/post?u=c5e9ceca50a5ee5da2cce5b04&amp;id=8d50a0b263" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+						
+					<div class="mc-field-group">
+						<label for="mce-EMAIL">Your email <span class="asterisk">*</span></label>
+						<input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL" placeholder="you@whatever.com">
+					</div>
+					<div class="mc-field-group">
+						<label for="mce-FNAME">Your first name <span class="asterisk">*</span></label>
+						<input type="text" value="" name="FNAME" class="required" id="mce-FNAME" placeholder="John">
+					</div>
+					<div id="mce-responses" class="clear">
+						<div class="response" id="mce-error-response" style="display:none"></div>
+						<div class="response" id="mce-success-response" style="display:none"></div>
+					</div>
+					<div class="clear" style="padding-top:15px;">
+						<input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button" style="margin:0;">
+					</div>
+					</form>
+				</div>
+				<!--End mc_embed_signup-->
+
+				</div>
+
+				<div class="5u">
+				<h3>Follow WordPress Leeds for updates via...</h3>		
+				<p>We can be grabbed via <a href="http://twitter.com/wpleeds">Twitter</a> but you can also <a href="mailto:tim@avvisi.co.uk">email Tim direct</a> and he can pass any messages on to the right people.</p>
+					
+				</div>				
+			</div>						
+		</div>
+	</article>
+
+<footer>
+	<p id="copyright">
+		&copy; Based on a template by <a href="http://kimb.me">Kimb Jones</a> who based it on a HTML template by the wonderful folk at <a href="http://html5up.net/">HTML5 UP</a> hosted on <a href="http://pages.github.com">GitHub</a> (sadly not powered by WordPress, yet...)
+	</p>
+</footer>
+
+</div>
+
 <?php wp_footer(); ?>
+
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-53047705-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
 
 </body>
 </html>
